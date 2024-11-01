5eae0755f6943590cfb569e50532f878c8b3adc3
Bogdan <bogdan@Bogdans-MBP.local>
2024-11-01 20:54:39 +0500

    Alabaster colorscheme

--- init.lua
+++ init.lua
@@ -97,6 +97,10 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- colorscheme
+vim.o.background = 'light'
+vim.cmd 'colorscheme alabaster'
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.

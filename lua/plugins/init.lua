f791beb49b3df71761530f28a3ce5e0c4a34eb44
Bogdan <bogdan@Bogdans-MBP.local>
2024-11-03 21:14:18 +0500

    cleaned up comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,23 +1,13 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  -- NOTE: Plugins can also be added by using a table, with the first argument being the link and the following keys can be used to configure plugin behavior/loading/etc.
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
   -- Use `opts = {}` to force a plugin to be loaded.
-  --
 
   -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  -- options to `gitsigns.nvim`. This is equivalent to the following Lua: require('gitsigns').setup({ ... })
+
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes see `:help gitsigns` to understand what the configuration keys do
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {

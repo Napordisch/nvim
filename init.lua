7037ab0045df73e0794aedcd573390e554f27c88
Bogdan <bogdan@Bogdans-MBP.local>
2024-11-04 13:06:46 +0500

    no terminal mode espace thing

--- init.lua
+++ init.lua
@@ -55,10 +55,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' }) -- Diagnostic keymaps
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier for people to discover. Otherwise, you normally need to press <C-\><C-n>, which is not what someone will guess without a bit more experience.
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- move with arrows
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
@@ -84,7 +80,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-require('set-lazy')
+require 'set-lazy'
 
 -- [[plugins]]
 --

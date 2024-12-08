93a73b52808cb0b7e207093ddc8a040c375b457b
Bogdan <bogdan@Bogdans-MBP.local>
2024-12-08 16:32:57 +0500

    plugin structure

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,9 @@
-return {
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+return { -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   -- NOTE: Plugins can also be added by using a table, with the first argument being the link and the following keys can be used to configure plugin behavior/loading/etc.
 
   -- Use `opts = {}` to force a plugin to be loaded.
 
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua: require('gitsigns').setup({ ... })
+  -- Here is a more advanced example where we pass configuration options to `gitsigns.nvim`. This is equivalent to the following Lua: require('gitsigns').setup({ ... })
 
   { -- Adds git related signs to the gutter, as well as utilities for managing changes see `:help gitsigns` to understand what the configuration keys do
     'lewis6991/gitsigns.nvim',
@@ -89,7 +87,6 @@ return {
     },
   },
 
-  -- BOOKMARK
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
@@ -607,31 +604,11 @@ return {
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  {import = 'plugins.files'},
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 }

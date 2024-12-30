a7732d867caa348c89c3b820297fb7080232c119
Napordisch <khamidullin.2005@gmail.com>
2024-12-31 01:14:54 +0500

    removed comments in lazy file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,3 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -18,7 +17,6 @@ vim.opt.runtimepath:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
 require("lazy").setup({
     spec = { import = "plugins" },
 })

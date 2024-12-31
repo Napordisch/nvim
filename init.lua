17e379492c6f875e4fbdf07372c26ea470cf5828
Napordisch <khamidullin.2005@gmail.com>
2024-12-31 18:06:25 +0500

    lua lsp

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 vim.keymap.set("n", "<space>x", ":.lua<CR>") -- execute one line
 vim.keymap.set("v", "<space>x", ":lua<CR>") -- execute selection
+
+vim.keymap.set('n', 'grn', vim.lsp.buf.rename) -- rename variable
+vim.keymap.set('n', 'grr', vim.lsp.buf.references) -- find references
+
 vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.wrap = true

edcae7db3075c0f649eca9da4b4b6836002a8329
Napordisch <khamidullin.2005@gmail.com>
2025-01-05 04:48:22 +0500

    softtabstop 4 and sidecsrolloff instead of wrapping

--- init.lua
+++ init.lua
@@ -5,10 +5,16 @@ vim.keymap.set('n', 'grn', vim.lsp.buf.rename) -- rename variable
 vim.keymap.set('n', 'grr', vim.lsp.buf.references) -- find references
 vim.g.mapleader = "<Space>"
 
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-vim.opt.wrap = true
+vim.opt.linebreak = false
+vim.opt.breakindent = false
+vim.opt.wrap = false
+vim.opt.sidescrolloff = 16
+
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+vim.opt.relativenumber = true
+vim.opt.number = true
 
 
 require('config.highlight_on_yank')

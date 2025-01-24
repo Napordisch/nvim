c814e25058602246c737eb182304d6e2b1f0d694
Napordisch <khamidullin.2005@gmail.com>
2025-01-24 21:04:59 +0500

    removed mini and added quickfix list navigation with meta j and k

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@ vim.keymap.set("v", "<space>x", ":lua<CR>") -- execute selection
 vim.keymap.set('n', 'grn', vim.lsp.buf.rename) -- rename variable
 vim.keymap.set('n', 'grr', vim.lsp.buf.references) -- find references
 vim.g.mapleader = " "
+vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>')
+vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>')
 
 vim.opt.linebreak = false
 vim.opt.breakindent = false
@@ -15,7 +17,6 @@ vim.opt.softtabstop = 4
 
 vim.opt.relativenumber = true
 vim.opt.number = true
-
 
 require('config.highlight_on_yank')
 require('config.lazy')

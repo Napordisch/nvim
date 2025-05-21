ae816dafd249ce8b518cbda4a337798303cf6062
Napordisch <khamidullin.2005@gmail.com>
2025-05-21 19:45:47 +0500

    harpoon

--- init.lua
+++ init.lua
@@ -1,11 +1,7 @@
 vim.keymap.set("n", "<space>x", ":.lua<CR>") -- execute one line
 vim.keymap.set("v", "<space>x", ":lua<CR>") -- execute selection
 
-vim.keymap.set('n', 'grn', vim.lsp.buf.rename) -- rename variable
-vim.keymap.set('n', 'grr', vim.lsp.buf.references) -- find references
 vim.g.mapleader = " "
-vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>')
-vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>')
 
 vim.opt.linebreak = true
 vim.opt.breakindent = true
@@ -16,8 +12,9 @@ vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 
-vim.opt.relativenumber = true
-vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.number = false
 
 require('config.highlight_on_yank')
 require('config.lazy')
+require('config.harpoon')
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,14 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
+
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' }) -- Find Files
+
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' }) -- Find with Grep
+
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' }) -- Find Buffers
+
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' }) -- Find Help
+
   end
 }
